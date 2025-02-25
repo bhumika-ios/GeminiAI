05b87fd0656a3b9d65d759155ec5c0d2cac0f67e
Bhumika Patel <bhumika.iosdeveloper@gmail.com>
2025-02-25 17:43:50 +0530

    Initial Commit

---
+++ GeminiAI/GeminiAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAIApp.swift
+//  GeminiAI
+//
+//  Created by Bhumika Patel on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
